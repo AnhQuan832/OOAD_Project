c81ddb94e741b98693d4321e9a1e6aabf80239cb
PHoangLe <lehoangphuc180502@gmail.com>
2023-01-30 16:02:59 +0700

    Implement FireBase connection, Push and Retrieve Image from FireBase

---
+++ FireBaseConnection.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System.Windows.Forms;
+using WinFormsApp1;
+using System.Drawing;
+using System.Reflection;
+using OOAD_Project.Properties;
+
+namespace OOAD_Project
+{
+    internal class FireBaseConnection
+    {
+        private static IFirebaseConfig _config = new FirebaseConfig
+        {
+            AuthSecret = "FDU6QrUwmvIyIdW0rXt7ZXyNiTK988aUC5LLQQB4",
+            BasePath = "https://rental-disc-store-management-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        };
+
+        public IFirebaseClient client = new FireSharp.FirebaseClient(_config);
+
+        public async void PushImage(PictureBox imgBox, String ImagePath)
+        {
+            //Push  - Picturebox  => ms => byte Array => toBase64String
+
+            MemoryStream ms = new MemoryStream();
+            imgBox.Image.Save(ms, ImageFormat.Jpeg);
+
+            byte[] a = ms.GetBuffer();
+
+            string output = Convert.ToBase64String(a);
+
+            var data = new Image_Modal
+            {
+                Img = output
+            };
+
+            SetResponse response = await client.SetTaskAsync(ImagePath, data);
+            Image_Modal result = response.ResultAs<Image_Modal>();
+
+            imgBox.Image = null;
+
+            MessageBox.Show("Image pushed");
+        }
+
+        public async void RetrieveImage(PictureBox imgBox, String ImagePath)
+        {
+            //Retrieve base64 => byte[] => ms => bitmap
+
+            FirebaseResponse response = await client.GetTaskAsync(ImagePath);
+            if (response.Body != "null")
+            {
+                Image_Modal image = response.ResultAs<Image_Modal>();
+
+                byte[] b = Convert.FromBase64String(image.Img);
+
+                MemoryStream ms = new MemoryStream();
+                ms.Write(b, 0, Convert.ToInt32(b.Length));
+
+                Bitmap bm = new Bitmap(ms, false);
+                ms.Dispose();
+
+                imgBox.Image = bm;
+            }
+            else
+            {
+                imgBox.Image = Resources.Cream_logo;
+            }
+            
+
+            
+        }
+    }
+}
--- UserControls/UsCtr_UserProfilecs.cs
+++ UserControls/UsCtr_UserProfilecs.cs
@@ -1,12 +1,48 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace OOAD_Project
 {
     public partial class UsCtr_UserProfilecs : UserControl
     {
+        FireBaseConnection fireBaseConnection = new FireBaseConnection();
+
         public UsCtr_UserProfilecs()
         {
             InitializeComponent();
+
+            TestConnectFireBase();
+            RetrieveAvatar();
+        }
+
+        private void RetrieveAvatar()
+        {
+            fireBaseConnection.RetrieveImage(avatarImgBox, "Avatar/" + fLogin.ID);
+        }
+
+        private void TestConnectFireBase()
+        {
+            if (fireBaseConnection.client != null)
+            {
+                MessageBox.Show("Firebase connected!");
+            }
+        }
+
+        private void guna2CirclePictureBox1_Click(object sender, System.EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Choose image";
+            ofd.Filter = "Image Files (*.bmp;*.png;*.jpg)|*.bmp;*.png;*.jpg";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Image img = new Bitmap(ofd.FileName);
+                avatarImgBox.Image = img.GetThumbnailImage(360, 200, null, new IntPtr());
+
+                fireBaseConnection.PushImage(avatarImgBox, "Avatar/" + fLogin.ID);
+                fireBaseConnection.RetrieveImage(avatarImgBox, "Avatar/" + fLogin.ID);
+            }
         }
     }
 }
